12af626c5633d411768f85909b570896444b373b
Jeff Cohen <jeff@codeacademy.org>
2012-08-23 10:25:53 -0500

    Delete items via Ajax

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -78,6 +78,7 @@ class NotesController < ApplicationController
     @note.destroy
 
     respond_to do |format|
+      format.js
       format.html { redirect_to notes_url }
       format.json { head :no_content }
     end
